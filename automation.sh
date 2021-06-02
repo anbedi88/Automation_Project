51ee54ff620f213b4f24de69b8d763d26a8b9e74
anbedi88 <83181809+anbedi88@users.noreply.github.com>
2021-06-02 17:47:27 -0500

    Add files via upload
    V2

--- automation.sh
+++ automation.sh
@@ -22,7 +22,25 @@ fi
 TIMESTAMP=$(date '+%d%m%Y-%H%M%S')
 tar -cvf /tmp/$NAME-httpd-logs-$TIMESTAMP.tar /var/log/apache2/*.log
 
+filen=`du -h /tmp/$NAME-httpd-logs-$TIMESTAMP.tar`
+size=`echo $filen |cut -d " " -f1`
+
 aws s3 \
 cp /tmp/$NAME-httpd-logs-${TIMESTAMP}.tar \
 s3://${S3BUCKET}/$NAME-httpd-logs-${TIMESTAMP}.tar
 
+ls -ltrh /tmp/*ANKIT*.tar|cut -d " " -f5,10 > /tmp/text
+
+if [[ -f /var/www/html/inventory.html ]]; then
+   echo -e "httpd-logs\t $TIMESTAMP \ttar \t $size" >>/var/www/html/inventory.html
+else
+   echo -e "Log Type \tTime Created \t\tType \t Size" > /var/www/html/inventory.html
+   while read p; do
+      secstr=`echo $p |cut -d " " -f2` 
+      firstr=`echo $p|cut -d " " -f1`
+      LOGTY=`echo $secstr |cut -d "-" -f2,3`
+      TIMECR=`echo $secstr|cut -d "-" -f4,5|cut -d "." -f1`
+      TYPE=`echo $secstr|cut -d "." -f2`
+      echo -e "$LOGTY \t$TIMECR \t$TYPE \t$firstr" >>/var/www/html/inventory.html
+   done </tmp/text
+fi
